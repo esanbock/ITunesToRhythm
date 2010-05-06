18c1a888f86efa0f1e98d71bf0e51bbc3c0e7ac1
douglas <douglas@aee2d1a1-4426-0410-a5f0-a01be3619014>
2010-05-06 17:18:45 +0000

    indent problems
    git-svn-id: https://www.esanbock.com/svn/repos/Python/ITunesToRhythm@795 aee2d1a1-4426-0410-a5f0-a01be3619014

--- songparser.py
+++ songparser.py
@@ -1,12 +1,12 @@
 import libxml2
 
 class BaseSong:
-        def __init__(self, song):
-                self.artist = "Unknown"
-                self.album = "Unknown"
-                self.title = "Unknown"
-                self.size = "Unknown"
-				self.rating = 0
+	def __init__(self, song):
+		self.artist = "Unknown"
+		self.album = "Unknown"
+		self.title = "Unknown"
+		self.size = "Unknown"
+		self.rating = 0
 
 class BaseLibraryParser:
 	def __init__(self, location):
