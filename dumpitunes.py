fea182b391eefff527933b3fd88f83f1fe993838
douglas <douglas@aee2d1a1-4426-0410-a5f0-a01be3619014>
2010-05-08 03:46:15 +0000

    Created the web page
    git-svn-id: https://www.esanbock.com/svn/repos/Python/ITunesToRhythm@853 aee2d1a1-4426-0410-a5f0-a01be3619014

--- dumpitunes.py
+++ dumpitunes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
-# iTunesToRhythm is free software; you can redistribute it and/or modify
+#Copyright @ 2010 Douglas Esanbock
+#iTunesToRhythm is free software; you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation; either version 3 of the License, or
 #(at your option) any later version.
