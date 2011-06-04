f94b1fd09bef601c7d191c8ff245c4390d159bdc
douglas <douglas@aee2d1a1-4426-0410-a5f0-a01be3619014>
2011-06-04 03:42:17 +0000

    wmp play counts
    git-svn-id: https://www.esanbock.com/svn/repos/Python/ITunesToRhythm@966 aee2d1a1-4426-0410-a5f0-a01be3619014

--- dumpwmp.py
+++ dumpwmp.py
@@ -32,8 +32,7 @@ class WMPSong(BaseSong):
 			except ValueError:
 				print WMPSong.getItemInfo("UserRating")
 				self.rating = 0
-			self.playcount = WMPSong.getItemInfo("PlayCount")
-			self.playcount = atoi(self.playcount)
+			self.playcount = int(WMPSong.getItemInfo("PlayCount"))
 			self.filePath = WMPSong.sourceURL
 
                 
