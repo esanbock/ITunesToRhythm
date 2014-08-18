c1092ea934414c33bb4ee35b7b41cbd2cef74393
esanbock <douglas@esanbock.com>
2014-08-17 21:27:18 -0500

    tabfix

--- dumpwmp.py
+++ dumpwmp.py
@@ -23,7 +23,7 @@ from pywintypes import com_error
 from songparser import BaseSong, BaseLibraryParser
 
 class WMPSong(BaseSong):
-        def __init__(self, WMPSong):
+	def __init__(self, WMPSong):
 			self.wmpNode = WMPSong
 			self.artist = WMPSong.getItemInfo("WM/AlbumArtist")
 			self.album = WMPSong.getItemInfo("WM/AlbumTitle")
@@ -32,61 +32,61 @@ class WMPSong(BaseSong):
 			try:
 				self.rating = int(WMPSong.getItemInfo("UserRating"))
 			except ValueError:
-				print WMPSong.getItemInfo("UserRating")
+				print( WMPSong.getItemInfo("UserRating") )
 				self.rating = 0
 			try:
 				self.playcount = int(WMPSong.getItemInfo("PlayCount"))
 			except ValueError:
-				print "WARNING - No playcount"
+				print( "WARNING - No playcount" )
 				self.playcount = 0
 			self.filePath = WMPSong.sourceURL
 
-                
-        def setRating(self, rating):
-                self.wmpNode.setItemInfo("UserRating", rating)
+		
+	def setRating(self, rating):
+		self.wmpNode.setItemInfo("UserRating", rating)
 
-        def setPlaycount(self, playcount):
-                self.wmpNode.setItemInfo("UserPlaycount", playcount)
+	def setPlaycount(self, playcount):
+		self.wmpNode.setItemInfo("UserPlaycount", playcount)
 
 class WMPParser(BaseLibraryParser):
-        def __init__(self):
-                self.wmp = win32com.client.Dispatch("WMPlayer.OCX")
-                self.cachedSongs = None;
+	def __init__(self):
+		self.wmp = win32com.client.Dispatch("WMPlayer.OCX")
+		self.cachedSongs = None;
 
-        def getSongs(self):
-                if self.cachedSongs is not None:
-                        return self.cachedSongs
-                songs = self.wmp.mediaCollection.getAll()
-                result = []
-                try:
-                        for s in songs:
-                                wmpSong = WMPSong(s)
-                                result.append(wmpSong)
-                except com_error as badio:
-                        print "\t parsing stopped due to error with " + str(len(result)) + " songs"
-                self.cachedSongs = result;
-                return result;
+	def getSongs(self):
+		if self.cachedSongs is not None:
+			return self.cachedSongs
+		songs = self.wmp.mediaCollection.getAll()
+		result = []
+		try:
+			for s in songs:
+				wmpSong = WMPSong(s)
+				result.append(wmpSong)
+		except com_error as badio:
+			print( "\t parsing stopped due to error with " + str(len(result)) + " songs" )
+		self.cachedSongs = result;
+		return result;
 
-        def save(self):
-                pass
+	def save(self):
+		pass
 
-        def InitConsole(self):
-                print "windows console"
-                if sys.stdout.encoding != 'cp850':
-                        sys.stdout = codecs.getwriter('cp850')(sys.stdout, 'strict')
+	def InitConsole(self):
+		print( "windows console" )
+		if sys.stdout.encoding != 'cp850':
+			sys.stdout = codecs.getwriter('cp850')(sys.stdout, 'strict')
 
 
 def main(argv):
-        print "Reading from Windows Media Player"
-        parser = WMPParser()
+	print( "Reading from Windows Media Player" )
+	parser = WMPParser()
 
-        allSongs = parser.getSongs()
+	allSongs = parser.getSongs()
 
-        for song in allSongs:
-                try:
-                        print song.artist + " - " + song.album + " - " + song.title + " - " + str(song.size) + ", " + str(song.playcount)
-                except:
-                        print "\tunable to print song name"
-                        
+	for song in allSongs:
+		try:
+			print( song.artist + " - " + song.album + " - " + song.title + " - " + str(song.size) + ", " + str(song.playcount) )
+		except:
+			print( "\tunable to print song name" )
+			
 if __name__ == "__main__":
-        main(sys.argv)
+	main(sys.argv)
